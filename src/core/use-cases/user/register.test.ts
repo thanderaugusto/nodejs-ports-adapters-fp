c308617cbc7475f6a0fdeb6d1f20ff79f307f61a
Thander Augusto <thander-99@hotmail.com>
2023-01-10 09:46:02 -0300

    fix register

--- src/core/use-cases/user/register.test.ts
+++ src/core/use-cases/user/register.test.ts
@@ -1,22 +1,23 @@
-import * as E from 'fp-ts/Either'
-import * as TE from 'fp-ts/TaskEither'
-import { pipe } from 'fp-ts/function'
-import { register, OutsideRegister } from './register'
-import { CreateUser } from '@/core/types/user'
-const registerOK: OutsideRegister = async (data) => {
-  return `Usuário  ${data.username} cadastrado com sucesso!`
-}
+import { CreateUser } from '@/core/types/user';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
+import { OutsideRegister, register } from './register';
+const registerOK: OutsideRegister<string> = async (data) => {
+  return `Usuário  ${data.username} cadastrado com sucesso!`;
+};
 
 const data: CreateUser = {
   username: 'test',
   email: 'test@test.com',
   password: '123456',
-}
+};
 
 it('Deveria cadastrar um usuário com sucesso', async () => {
   return pipe(
     data,
     register(registerOK),
-    TE.map(result => expect(result).toBe(`Usuário  ${data.username} cadastrado com sucesso!`)),
-  )()
-})
+    TE.map((result) =>
+      expect(result).toBe(`Usuário  ${data.username} cadastrado com sucesso!`)
+    )
+  )();
+});
--- src/core/use-cases/user/register.ts
+++ src/core/use-cases/user/register.ts
@@ -1,18 +1,14 @@
-import * as TE from 'fp-ts/TaskEither'
-import * as E from 'fp-ts/Either'
-import { pipe } from 'fp-ts/function'
-import { CreateUser } from '@/core/types/user'
+import { CreateUser } from '@/core/types/user';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/TaskEither';
 
-export type OutsideRegister<A> = (data: CreateUser) => Promise<unknown>
+export type OutsideRegister<A> = (data: CreateUser) => Promise<A>;
 
-type Register = <A>(outsideRegister: OutsideRegister<A>) =>
-    (data: CreateUser) => TE.TaskEither<Error, A>
+type Register = <A>(
+  outsideRegister: OutsideRegister<A>
+) => (data: CreateUser) => TE.TaskEither<Error, A>;
 
 export const register: Register = (outsideRegister) => (data) => {
-  return pipe(
-    TE.tryCatch(
-      () => outsideRegister(data),
-      E.toError,
-    ),
-  )
-}
+  return pipe(TE.tryCatch(() => outsideRegister(data), E.toError));
+};
